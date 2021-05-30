b2654e5a0a0844079444cec9207ce7f6d249b747
joelvitorniino <joelprofessor13@gmail.com>
2021-05-30 14:15:04 -0300

    adding new modifications on index and package

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const path = require("path");
-const bodyParder = require("body-parser");
 const handlebars = require("express-handlebars");
 const app = express();
 
  // Configuracao para express aceitar Json!
-app.use(bodyParder.json())
-app.use(bodyParder.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // Usando os arquivos do front!
 app.use(express.static(path.join(__dirname)));
  //Engine Handlebars
@@ -19,4 +18,4 @@ require("./back-end/controls/userControlsAuth")(app);
 
 app.listen(3003, () => {
   console.log("server up port:", 3003);
-})
+});
